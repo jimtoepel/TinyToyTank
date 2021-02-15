fc3227336d04879518ec776abba67dd3c7ac8c04
JToeps <jim.toepel@gmail.com>
2021-02-15 11:42:58 -0800

    Fixed animation bug

--- TinyToyTank/ViewController.swift
+++ TinyToyTank/ViewController.swift
@@ -11,24 +11,55 @@ import RealityKit
 class ViewController: UIViewController {
     
     var tankAnchor: TinyToyTank._TinyToyTank?
+    var isActionPlaying: Bool = false
     @IBOutlet var arView: ARView!
     
     @IBAction func tankRightPressed(_ sender: Any) {
+        if self.isActionPlaying {
+            return
+        } else {
+            self.isActionPlaying = true
+        }
         tankAnchor!.notifications.tankRight.post()
     }
     @IBAction func tankForwardPressed(_ sender: Any) {
+        if self.isActionPlaying {
+            return
+        } else {
+            self.isActionPlaying = true
+        }
         tankAnchor!.notifications.tankForward.post()
     }
     @IBAction func tankLeftPressed(_ sender: Any) {
+        if self.isActionPlaying {
+            return
+        } else {
+            self.isActionPlaying = true
+        }
         tankAnchor!.notifications.tankLeft.post()
     }
     @IBAction func turretLeftPressed(_ sender: Any) {
+        if self.isActionPlaying {
+            return
+        } else {
+            self.isActionPlaying = true
+        }
         tankAnchor!.notifications.turretLeft.post()
     }
     @IBAction func turretRightPressed(_ sender: Any) {
+        if self.isActionPlaying {
+            return
+        } else {
+            self.isActionPlaying = true
+        }
         tankAnchor!.notifications.turretRight.post()
     }
     @IBAction func cannonFirePressed(_ sender: Any) {
+        if self.isActionPlaying {
+            return
+        } else {
+            self.isActionPlaying = true
+        }
         tankAnchor!.notifications.cannonFire.post()
     }
     
@@ -39,6 +70,10 @@ class ViewController: UIViewController {
         // Load the "Tank" scene from the "TTT" Reality File
         tankAnchor = try! TinyToyTank.load_TinyToyTank()
         tankAnchor!.cannon?.setParent(tankAnchor!.tank, preservingWorldTransform: true)
+        
+        tankAnchor?.actions.actionComplete.onAction = { _ in
+            self.isActionPlaying = false
+        }
         
         // Add the tank anchor to the scene
         arView.scene.anchors.append(tankAnchor!)
